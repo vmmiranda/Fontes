0ba4d0b3978b46216901d05cb092a40677155be3
vmmiranda <vitor.vmiranda@gmail.com>
2021-02-14 18:15:40 -0300

    Add files via upload

---
+++ Lojas_Proximidade/FrmBuscaLojas.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Lojas_Proximidade
+{
+    partial class FrmBuscaLojas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtLojas = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPlano = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPosicao = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbResultado = new System.Windows.Forms.ListBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(358, 161);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.txtLojas);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.txtPlano);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.txtPosicao);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnCalcular);
+            this.panel1.Location = new System.Drawing.Point(12, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(745, 197);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtLojas
+            // 
+            this.txtLojas.Location = new System.Drawing.Point(108, 39);
+            this.txtLojas.Multiline = true;
+            this.txtLojas.Name = "txtLojas";
+            this.txtLojas.Size = new System.Drawing.Size(629, 101);
+            this.txtLojas.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(70, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Lojas:";
+            // 
+            // txtPlano
+            // 
+            this.txtPlano.Location = new System.Drawing.Point(447, 4);
+            this.txtPlano.Name = "txtPlano";
+            this.txtPlano.Size = new System.Drawing.Size(290, 20);
+            this.txtPlano.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(404, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Plano:";
+            // 
+            // txtPosicao
+            // 
+            this.txtPosicao.Location = new System.Drawing.Point(108, 3);
+            this.txtPosicao.Name = "txtPosicao";
+            this.txtPosicao.Size = new System.Drawing.Size(290, 20);
+            this.txtPosicao.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Posição do Cliente:";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.lbResultado);
+            this.panel2.Location = new System.Drawing.Point(12, 267);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(745, 123);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Informações";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resultado";
+            // 
+            // lbResultado
+            // 
+            this.lbResultado.FormattingEnabled = true;
+            this.lbResultado.Location = new System.Drawing.Point(3, 3);
+            this.lbResultado.Name = "lbResultado";
+            this.lbResultado.Size = new System.Drawing.Size(734, 108);
+            this.lbResultado.TabIndex = 0;
+            // 
+            // FrmBuscaLojas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "FrmBuscaLojas";
+            this.Text = "Busca de Lojas por Proximidade";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLojas;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtPlano;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPosicao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox lbResultado;
+    }
+}
+
---
+++ Lojas_Proximidade/FrmBuscaLojas.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lojas_Proximidade
+{
+    public partial class FrmBuscaLojas : Form
+    {
+        public FrmBuscaLojas()
+        {
+            InitializeComponent();
+        }
+        //Metodo que calcula as Lojas_Proximidade mais proximas em relação ao cliente.
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {              
+            //Verifica se os campos estão preenchidos
+            if (VerificaPreenchimento())
+            {
+                try
+                {
+                    //Transformando campo do TextBox para o array
+                    int[,] posicaoDoCliente = TransformaArrayPosicoes(txtPosicao.Text.Replace("[", "{").Replace("]", "}").Trim());
+                    int[,] lojas = TransformaArrayPosicoes(txtLojas.Text.Replace("[", "{").Replace("]", "}").Trim());
+                    int[,] plano = TransformaArrayPosicoes(txtPlano.Text.Replace("[", "{").Replace("]", "}").Trim());
+
+                    SortedList lojasSortedList = new SortedList();
+                    //Obtem lojas mais proximas
+                    lojasSortedList = LojasProximas(posicaoDoCliente, lojas, plano);
+
+                    //Verifica se os campos de plano , loja e cliente estão de acordo com as restrições
+                    if (VerificaRestricoes(posicaoDoCliente, lojas, plano))
+                    {
+                        lbResultado.Items.Clear();
+
+                        int i = 0;
+                        foreach (DictionaryEntry item in lojasSortedList)
+                        {
+                            if (i < 3)
+                            {
+                                int[,] arrlojas = (int[,])item.Value;
+                                lbResultado.Items.Add($"Loja: [{arrlojas[0, 0]},{arrlojas[0, 1]}] --> Distância: { item.Key}");
+                                i++;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Restrições dos parâmetros foram violadas");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Parâmetros estão com a formatação errada. Por favor corrija a formatação.");
+                    return;
+                }
+
+            }
+        }
+        //Transforma textbox em array 
+        int[,] TransformaArrayPosicoes(string parametro_posicoes)
+        {
+            string[] arrIndices = null;
+
+            arrIndices = parametro_posicoes.Split('}').ToArray();
+            int[,] arrRetorno = new int[arrIndices.Count() - 2, 2];
+            string[] arrPosicoes = new string[2];
+            bool bpassou_primeira_ocorrencia = false;
+
+            string retorno_indice;
+
+            for (int i = 0; i < arrIndices.Length; i++)
+            {
+                if (arrIndices[i].Contains(","))
+                {
+                    if (!bpassou_primeira_ocorrencia)
+                    {
+                        retorno_indice = arrIndices[i].Replace("{", string.Empty);
+                        bpassou_primeira_ocorrencia = true;
+                    }
+                    else
+                    {
+                        retorno_indice = arrIndices[i].Substring(1).Replace("{", string.Empty);
+                    }
+
+                    arrPosicoes = retorno_indice.Split(',').ToArray();
+
+                    arrRetorno[i, 0] = int.Parse(arrPosicoes[0]);
+                    arrRetorno[i, 1] = int.Parse(arrPosicoes[1]);
+                }
+
+            }
+            return arrRetorno;
+
+
+        }
+        //Verificar preenchimento dos campos 
+        bool VerificaPreenchimento()
+        {
+            if (string.IsNullOrEmpty(txtPosicao.Text) || (string.IsNullOrEmpty(txtPlano.Text)) || (string.IsNullOrEmpty(txtLojas.Text)))
+            {
+                MessageBox.Show("Campos de Posição do Cliente , Lojas e Plano devem ser informados.");
+                return false;
+            }
+            return true;
+        }
+        //Verificar restrições dos campos que foram passados
+        bool VerificaRestricoes(int[,] posicaoDoCliente, int[,] lojas, int[,] plano)
+        {
+            if (posicaoDoCliente.Length == 0 || lojas.Length == 0 || plano.Length == 0)
+            {
+                return false;
+            }
+            else if (plano[0, 0] < 0 || plano[0, 0] > 1000 || plano[0, 0] < posicaoDoCliente[0, 0] || posicaoDoCliente[0, 0] < 0)
+            {
+                return false;
+            }
+            else if (plano[0, 1] < 0 || plano[0, 1] > 1000 || plano[0, 1] < posicaoDoCliente[0, 1] || posicaoDoCliente[0, 1] < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        //Função que retorna lojas mais proximas
+        static SortedList LojasProximas(int[,] posicaoDoCliente, int[,] lojas, int[,] plano)
+        {
+            int[] retorno = new int[3];
+            double distancia = 0;
+
+            SortedList lojasSortedList = new SortedList();
+
+            for (int linha = 0; linha < lojas.GetLength(0); linha++)
+            {
+                distancia = Math.Sqrt(Math.Pow(posicaoDoCliente[0, 0] - lojas[linha, 0], 2) + Math.Pow(posicaoDoCliente[0, 1] - lojas[linha, 1], 2));
+
+                int[,] item = { { lojas[linha, 0], lojas[linha, 1] } };
+                lojasSortedList.Add(distancia, item);
+            }
+
+            return lojasSortedList;
+        }
+    }
+}
