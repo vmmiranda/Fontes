94373deca6109b88b4f41974fa39e8008fa44ece
vmmiranda <vitor.vmiranda@gmail.com>
2021-02-14 19:18:11 -0300

    Add files via upload

---
+++ WebAPI_Moeda/WebAPI_Moeda/Controllers/MoedaController.cs
@@ -0,0 +1,52 @@
+﻿using Servico_Domain.Entities;
+using Servico_Domain.Interfaces;
+using Servico_Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI_Servicos.Controllers
+{
+    
+    public class MoedaController : ApiController
+    {
+        static readonly IMoedaService repositorio = new MoedaService();
+        /// <summary>
+        /// Método que converte o valor nas demais moedas cadastradas na base.
+        /// </summary>  
+        /// /// <remarks>
+        /// Retorna uma lista de produtos com informações de ID Produto, Nome da moeda convertida e preço na moeda convertida.
+        /// 
+        /// Exemplo de requisição:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///     "Id": 1,
+        ///     "Nome": "Raquete",
+        ///     "Categoria": "Tenis",
+        ///     "Preco": 50.43,
+        ///     "Nome_Moeda": "BRL"   
+        ///     }
+        ///
+        /// </remarks>
+
+    public HttpResponseMessage PostOutrasMoedas(ProdutoETT item_Produto)
+        {           
+            List<ProdutoMoedaETT> lstProdutoMoeda = new List<ProdutoMoedaETT>();
+            lstProdutoMoeda = repositorio.CalcularOutrasMoedas(item_Produto);
+
+            if (lstProdutoMoeda.Count == 0)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Não foi possivel converter em outras moedas.");
+            }
+            else
+            {
+                return Request.CreateResponse<List<ProdutoMoedaETT>>(HttpStatusCode.OK, lstProdutoMoeda);
+            }
+        }
+       
+    }
+}
---
+++ WebAPI_Moeda/WebAPI_Moeda/Controllers/ProdutosController.cs
@@ -0,0 +1,51 @@
+﻿using Servico_Domain.Entities;
+using Servico_Domain.Interfaces;
+using Servico_Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI_Servicos.Models;
+
+namespace WebAPI_Servicos.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProdutosController : ApiController
+    {
+        static readonly IProdutoService repositorio = new ProdutoService();
+
+        /// <summary>
+        /// Método que obtêm os produtos cadastrados na base.
+        /// </summary>               
+        /// <remarks>Retorna uma lista de produtos com informações de ID, Nome , Categoria e Preço</remarks>
+        public HttpResponseMessage GetAllProdutos()
+        {
+
+            List<ProdutoViewModel> lstProduto = new List<ProdutoViewModel>();
+
+            using (var ctx = new dbBaseEntities())
+            {
+                lstProduto = ctx.Produto
+                            .Select(s => new ProdutoViewModel()
+                            {
+                                Id_Produto = s.Id_Produto,
+                                Categoria = s.Categoria,
+                                Nome = s.Nome,
+                                Descricao = s.Descricao,
+                                Preco = s.Preco
+                            }).ToList<ProdutoViewModel>();
+
+                if (lstProduto.Count == 0)
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Não foi possivel obter os produtos.");
+                }
+
+                return Request.CreateResponse<List<ProdutoViewModel>>(HttpStatusCode.OK, lstProduto);
+            }
+           
+        }      
+             
+    }
+}
